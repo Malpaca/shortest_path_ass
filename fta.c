a406faaa73c7eca6c75ac3c6f6e723ec5860cd23
yliu17 <yliu17@simons-rock.edu>
2019-05-05 12:21:53 -0400

    done

--- fta.c
+++ fta.c
@@ -1,4 +1,5 @@
 #include "dijkstra.h"
+#include "ctype.h"
 
 int name_map(char* name){
     int a = (name[0]-'A')*26*26 + (name[1]-'A')*26 + (name[2]-'A');
@@ -32,6 +33,9 @@ void free_indices(char ** indices, int total){
 }
 
 int extract_vertex(char * filename, int * arr){
+    for(int i = 0; i < 26*26*26; i++){
+        arr[i] = -1;
+    }
     FILE * fp;
     fp = fopen(filename, "r");
     if(fp == NULL) printf("Error opening file %s\n",filename);
@@ -64,6 +68,10 @@ void distance(char * filename, int total, char a[4], char b[4], int * names, cha
     //start and end string mapping recorded
     int start = names[name_map(a)];
     int end = names[name_map(b)];
+    if (start == -1 || end == -1){
+        printf("Error, airport name not in record, please use airports command to see all airports\n");
+        return;
+    }
     //open file and extract undirected flight path as edge and translate into graph data structure
     FILE * fp;
     fp = fopen(filename, "r");
@@ -73,25 +81,33 @@ void distance(char * filename, int total, char a[4], char b[4], int * names, cha
     //read from file and add edge to the structure
     int dist; char depart[4], arrival[4];
     while(fscanf(fp, "%s %s %d", depart, arrival, &dist) != EOF) {
-        // printf("%d: %s:%d, %s:%d, %d\n", total, depart, names[name_map(depart)], arrival, names[name_map(arrival)],dist);
         add_edge(g, names[name_map(depart)], names[name_map(arrival)], dist);
-        // printf("%d,%d,%d,%d\n",
-        //                     g->vertices[names[name_map(depart)]]->e_len,
-        //                     g->vertices[names[name_map(depart)]]->e_size,
-        //                     g->vertices[names[name_map(depart)]]->dist,
-        //                     g->vertices[names[name_map(depart)]]->visited);
         add_edge(g, names[name_map(arrival)], names[name_map(depart)], dist);
-        // if (!strcmp(depart, "CLT") && dist == 613) break;
-        // printf("%d,%d,%d,%d\n", g->vertices[names[name_map(arrival)]]->e_len,
-        //                     g->vertices[names[name_map(arrival)]]->e_size,
-        //                     g->vertices[names[name_map(arrival)]]->dist,
-        //                     g->vertices[names[name_map(arrival)]]->visited);
     }
     fclose(fp);
     //run dijkstra
     dijkstra(g, start, end, total, indices);
     free_all(g, total);
-    // print_path(g, end);
+}
+
+void strupr(char * string){
+	int i = 0;
+	while (string[i] != '\0'){
+    	if (string[i] >= 'a' && string[i] <= 'z') {
+        	string[i] = string[i] - 32;
+    	}
+      	i++;
+	}
+}
+
+int name_check(char * start, char * end){
+    if (strlen(start) != 3 || strlen(end) != 3) return 0;
+    for(int i = 0; i < 3; i++){
+        if(!isalpha(start[i]) || !isalpha(end[i])) return 0;
+    }
+    strupr(start);
+    strupr(end);
+    return 1;
 }
 
 int main(int argc, char ** argv){
@@ -100,7 +116,7 @@ int main(int argc, char ** argv){
         return 1;
     }
     //map airport name to unique num
-    int names[26*26*26] = {-1};
+    int names[26*26*26];
     int total = extract_vertex(argv[1], names);
     char ** indices = map_index(argv[1], total);
     //read user input and process input
@@ -110,7 +126,7 @@ int main(int argc, char ** argv){
     while(fgets(input, 20, stdin)) {
         command = strtok(input, " \n");
         start = strtok(NULL," \n");
-        end = strtok(NULL," \n");
+        end = start != NULL ? strtok(NULL," \n") : NULL;
         //quiting
         if (!strcasecmp(command, "quit")) {
             printf("Exiting\n");
@@ -118,7 +134,12 @@ int main(int argc, char ** argv){
         }
         //help command
         else if (!strcasecmp(command, "help")) {
-            printf("Help Command\n\n");
+            printf("Following are the avaliable command:\n\
+                    quit:exit the program\n\
+                    help: show all avaliable command and format\n\
+                    airports: print all avaliable airports\n\
+                    distance <depart> <arrival>: print the shortest path and distance from depart to arrival\n\
+                    All command are case insensitive\n\n");
         }
         //print airport
         else if (!strcasecmp(command, "airports")) {
@@ -126,7 +147,9 @@ int main(int argc, char ** argv){
         }
         //distance
         else if (!strcasecmp(command, "distance")) {
-            distance(argv[2], total, start, end, names, indices);
+            if (start && end && name_check(start, end)) distance(argv[2], total, start, end, names, indices);
+            else printf("Wrong input, airport name must be only 3 english alphabet character.\n");
+            printf("\n");
         }
         else{
             printf("Invalid input, use \"help\" to see avaliable command\n\n");
